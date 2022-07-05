782e60f147c5b043c306db9bebc564188f472f56
Allan Kildare <allankildaredev@gmail.com>
2022-07-05 02:13:27 -0300

    chore: adding new endpoints

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -1,1 +1,4 @@
 export const GITHUB_API_ENDPOINT = import.meta.env.VITE_GITHUB_API_ENDPOINT
+export const USERS_ENDPOINT = GITHUB_API_ENDPOINT + '/users'
+export const REPOSITORIES_ENDPOINT = GITHUB_API_ENDPOINT + '/repos'
+export const BRANCHES = '/branches'
