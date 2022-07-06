fc5083bfbfd645fe74beff1ac6902a616231229f
Allan Kildare <allankildaredev@gmail.com>
2022-07-06 04:05:01 -0300

    refactor: improvement of enabled config, adding type of branch

--- src/services/getBranches.ts
+++ src/services/getBranches.ts
@@ -1,17 +1,18 @@
 import { useQuery } from 'react-query'
 import { apiClient } from '~/clients'
+import { Branch } from '~/types'
 import { BRANCHES, REPOSITORIES_ENDPOINT } from './endpoints'
 
-export function getBranches({ user, repoName, enabled }: { user: string, repoName: string, enabled: boolean}) {
+export function getBranches({ user, repoName }: { user?: string, repoName?: string }) {
     const url = REPOSITORIES_ENDPOINT + `/${user}/${repoName}` + BRANCHES
 
     const { data, ...restQuery } = useQuery('getBranches', async () => {
         return await apiClient.get(url)
     }, {
-        enabled: enabled
+        enabled: Boolean(user && repoName)
     })
 
-    const response = data?.data
+    const response = data?.data as Branch[]
 
     return { data: response, ...restQuery }
 }
