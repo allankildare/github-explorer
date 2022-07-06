54123fb88b51071b08093ac4b547916232c93616
Allan Kildare <allankildaredev@gmail.com>
2022-07-05 23:55:11 -0300

    feat: add commits string

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -1,4 +1,5 @@
-export const GITHUB_API_ENDPOINT = import.meta.env.VITE_GITHUB_API_ENDPOINT
-export const USERS_ENDPOINT = GITHUB_API_ENDPOINT + '/users'
-export const REPOSITORIES_ENDPOINT = GITHUB_API_ENDPOINT + '/repos'
 export const BRANCHES = '/branches'
+export const COMMITS = '/commits'
+export const GITHUB_API_ENDPOINT = import.meta.env.VITE_GITHUB_API_ENDPOINT
+export const REPOSITORIES_ENDPOINT = GITHUB_API_ENDPOINT + '/repos'
+export const USERS_ENDPOINT = GITHUB_API_ENDPOINT + '/users'
