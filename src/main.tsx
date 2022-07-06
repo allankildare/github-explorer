5f84192337d1b7ac3dadb784a8d2da1d0e727aa8
Allan Kildare <allankildaredev@gmail.com>
2022-07-06 02:19:52 -0300

    chore: avoid forbidden null assertion, implementing routes and adding dayjs locale configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 import { QueryClientProvider } from 'react-query'
+import dayjs from 'dayjs'
+import 'dayjs/locale/pt-br'
+import './index.css'
 import { queryClient } from './clients'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Main } from '~/ui/components'
+import { BranchesAndCommitsPage, RepositoriesPage } from '~/pages'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// dayjs config to brazilian portuguese
+dayjs.locale('pt-br')
+
+const rootElement = document.getElementById('root')
+
+if(rootElement)
+ReactDOM.createRoot(rootElement).render(
   <QueryClientProvider client={queryClient}>
-      <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/:user" element={<RepositoriesPage />} />
+        <Route path="/:user/:repository" element={<BranchesAndCommitsPage />} />
+      </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 )
